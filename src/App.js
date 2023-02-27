7cf484fc87fdf4f71816f00311c400b2fec0b96f
yolly98 <gianlucagemini98@gmail.com>
2023-02-27 01:03:58 +0100

    added itemMenu

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Login from './components/js/Login';
 import Alert from './components/js/Alert';
 import Navbar from './components/js/Navbar';
 import Item from './components/js/Item';
+import ItemMenu from './components/js/ItemMenu';
 
+import infoAlert from './images/infoAlert.png'
 import warningAlert from './images/warningAlert.png'
 import errorAlert from './images/errorAlert.png'
 import add from './images/add.png'
 import refresh from './images/refresh.png'
+import defaultWebSite from './images/defaultWebsite.png'
 
 class App extends Component{
 
@@ -20,11 +23,12 @@ class App extends Component{
         user: "",
         password: "",
         items: [],
+        itemMenu: -1,
         alert: {
             state: false,
             title: "example",
             text: "exmaple text",
-            image: warningAlert
+            image: infoAlert
         }
     }
 
@@ -37,13 +41,15 @@ class App extends Component{
     }
 
     getItems(){
+
+        // TODO request to database database
         let items = [];
         for(let i = 0; i < 20; i++){
             items.push(
                 {
                     id: i,
                     name: 'sito' + i,
-                    image: "https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196",
+                    image: defaultWebSite,
                     display: "flex"
                 }
             )
@@ -110,28 +116,79 @@ class App extends Component{
     }
 
     handleAddCard(){
-        //console.log("add card pressed");
-        /*this.setState({itemMenu: -2});
+        this.setState({itemMenu: -2});
         document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-        document.getElementById('blocker1').style.display = 'block';*/
-        this.openAlert("Add item", "aggiunta item", warningAlert);
+        document.getElementById('blocker1').style.display = 'block';
     }
 
     
     handleRefresh(){
         this.getItems();
-        this.openAlert("SUCCESS", "Elementi ricaricati", warningAlert);
+        this.openAlert("SUCCESS", "Elementi ricaricati", infoAlert);
     }
     
     handleOpenItem = item => {
-        //this.setState({itemMenu: this.state.cards.indexOf(card)});
-        //document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-        //document.getElementById('item-blocker').style.display = 'block';
-        this.openAlert("Open Item", item.name, warningAlert);
-    
+        this.setState({itemMenu: this.state.items.indexOf(item)});
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+        document.getElementById('item-blocker').style.display = 'block';
+        
+    }
+
+    handleCloseItem = () => {
+        this.setState({itemMenu: -1});
+        document.getElementsByTagName('body')[0].style.overflow = 'auto';
+        document.getElementById('item-blocker').style.display = 'none';
+    }
+
+    handleSaveItem = state => {
+        
+        let item = state.item;
+        let isNewItem = false;
+        let items = [...this.state.items];
+
+        if(!item.hasOwnProperty("id")){
+            isNewItem = true;
+            // item.id = -3;
+            let maxId = 0;
+            for(let _item of items)
+                if(_item.id > maxId)
+                    maxId = _item.id;
+            item.id = maxId + 1;
+        }
+        
+        item.name = document.getElementById("itemMenu-input-name").value;
+        item.image = document.getElementById("itemMenu-input-url").value;
+        if(item.image == "")
+            item.image = defaultWebSite;
+
+        console.log(item);
+        
+        if(items.length == 0)
+            items.push(item);
+        for(let i = 0; i < items.length; i++){
+            if(items[i].id == item.id){
+                items[i] = item;
+                break;
+            }
+            if(i == items.length - 1)
+                items.push(item);
+        }
+        
+        this.setState(
+            {itemMenu: -1, items},
+            function(){
+                this.openAlert("SUCCESS", "Salvataggio avvenuto con successo", infoAlert);
+                document.getElementsByTagName('body')[0].style.overflow = 'auto';
+                document.getElementById('item-blocker').style.display = 'none';
+            }
+          );
+        // TODO save item in database
     }
 
     handleDeleteItem = item => {
+
+        // TODO remove from database
+
         //this.setState({itemMenu: this.state.cards.indexOf(card)});
         //document.getElementsByTagName('body')[0].style.overflow = 'hidden';
         //document.getElementById('item-blocker').style.display = 'block';
@@ -140,7 +197,7 @@ class App extends Component{
         this.setState(
           {items},
           function(){
-            this.openAlert("SUCCESS", (item.name + " rimosso!"), warningAlert);
+            this.openAlert("SUCCESS", (item.name + " rimosso!"), infoAlert);
           }
         );
     
@@ -148,6 +205,8 @@ class App extends Component{
       
     render(){
         let page;
+        let alert;
+        let itemMenu;
 
         if(this.state.alert.state){
             alert = <Alert
@@ -184,6 +243,21 @@ class App extends Component{
         }
 
         else if (this.state.page == 'home'){
+            if(this.state.itemMenu >= 0){
+                itemMenu = <ItemMenu 
+                            onCancel = {this.handleCloseItem} 
+                            onSave = {this.handleSaveItem} 
+                            item = {this.state.items[this.state.itemMenu]} 
+                          />
+            }else if(this.state.itemMenu == -2){
+              itemMenu = <ItemMenu
+                            onCancel = {this.handleCloseItem} 
+                            onSave = {this.handleSaveItem} 
+                            item = {{}} 
+                          />
+            }
+            else
+              itemMenu = <></>
             page = <>
                     <div 
                         id="blocker" 
@@ -207,7 +281,7 @@ class App extends Component{
                             opacity: '0.4', 
                             position: 'fixed', 
                             top: '0', 
-                            zIndex: '9', 
+                            zIndex: '7', 
                             display: "none"
                         }} 
                     />
@@ -230,6 +304,7 @@ class App extends Component{
                         ))
                     }
                     </div>
+                    {itemMenu}
                     {alert}
             </>
 
---
+++ src/components/js/ItemMenu.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import '../style/ItemMenu.css'
+
+import cross from '../../images/delete.png'
+import defaultImage from '../../images/defaultWebsite.png';
+import showPassw from '../../images/showPassw.png';
+import hidePassw from '../../images/hidePassw.png';
+import copy from '../../images/copy.png';
+
+class ItemMenu extends Component{
+
+    state = {
+        item: this.props.item,
+        name: "",
+        username: "",
+        password: 0,
+        notes: "",
+        url: defaultImage
+    }
+
+    componentDidMount(){
+
+        if(!this.state.item.hasOwnProperty("name"))
+            return;
+        
+        // TODO request to server
+
+        let name = this.state.item.name;
+        let username = "test-username";
+        let password = "test-password";
+        let notes = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+        let image = this.state.item.image;
+
+        document.getElementById("itemMenu-input-name").value = name;
+
+        this.setState({ 
+            name,
+            username,
+            password,
+            notes,
+            image
+        });
+
+    }
+
+    copy(){
+
+        let passw = document.getElementById('itemMenu-input-password');
+        passw.type ='text';
+        passw.select();
+        passw.setSelectionRange(0,99999);//needed for mobile devices
+        document.execCommand("copy");
+        passw.type='password';
+    }
+
+    showPassw(){
+
+        let passw = document.getElementById("itemMenu-input-password");
+        let passwImage = document.getElementById("itemMenu-showPassw");
+        if(passw.type == 'text'){
+            passw.type = 'password';
+            passwImage.src = hidePassw; 
+        }
+        else{
+            passw.type = 'text';
+            passwImage.src = showPassw;
+        }
+    }
+
+    render(){
+        return(
+            <div id="itemMenu-section">
+                <div id="itemMenu-container">
+                    <div id="itemMenu-container2">
+                        <img src={cross} style={{width: '1.5rem', cursor: 'pointer'}} onClick={()=>this.props.onCancel()}/>
+                    </div>
+                    <div id="itemMenu-container3">
+                        <img id="itemMenu-image" src={this.state.item.image}/>
+                        <label className='itemMenu-label'>Nome</label>
+                        <input className="itemMenu-text" id="itemMenu-input-name" type="text" style={{marginBottom: '1rem'}}/>
+                        <label className='itemMenu-label'>Username</label>
+                        <input className="itemMenu-text" id="itemMenu-input-username" type="text" style={{marginBottom: '1rem'}}/>
+                        <label className='itemMenu-label'>Password</label>
+                        <div id="itemMenu-passwContainer">
+                            <input className="itemMenu-text" id="itemMenu-input-password" type="password" style={{marginBottom: '1rem'}}/>
+                            <img id="itemMenu-showPassw" src={hidePassw} onClick={() => this.showPassw()}></img>
+                            <img id="itemMenu-copy" src={copy} onClick={() => this.copy()}></img>
+                        </div>
+                        <label className='itemMenu-label'>Url Icona</label>
+                        <input  className="itemMenu-text" id="itemMenu-input-url" type="text" style={{marginBottom: '1rem'}}/>
+                        <label className='itemMenu-label'>Note</label>
+                        <textarea className="itemMenu-text" id="itemMenu-input-notes"></textarea>
+                    </div>
+                    <div id="itemMenu-container4">
+                        <button className="itemMenu-button" style={{borderRadius: '0.5rem'}} onClick={()=>this.props.onRemove(this.state)}>RIMUOVI</button>
+                        <button className="itemMenu-button" style={{borderRadius: '0.5rem'}} onClick={()=>this.props.onSave(this.state)}>SALVA</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ItemMenu;
